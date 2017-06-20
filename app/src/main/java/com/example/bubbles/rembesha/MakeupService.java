14d356a0f6eaaef9a1257ffae527946d339aff1e
Yonkokilasi <yonkokilasi@gmail.com>
2017-06-20 12:20:27 +0300

    Added parameters for the url

--- app/src/main/java/com/example/bubbles/rembesha/MakeupService.java
+++ app/src/main/java/com/example/bubbles/rembesha/MakeupService.java
@@ -1,13 +1,26 @@
 package com.example.bubbles.rembesha;
 
-import android.support.v7.widget.helper.ItemTouchHelper;
+import okhttp3.Callback;;
+
+import okhttp3.Call;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 /**
  * Created by bubbles on 6/20/17.
  */
 
 public class MakeupService {
-    public static final findMakeUp(String brand, String pType, String pCategory, ItemTouchHelper.Callback callback) {
-
+    public static void findMakeUp(String brand, String pType, String pCategory, Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.BRAND_PARAMETER,brand);
+        urlBuilder.addQueryParameter(Constants.CATEGORY_PARAMETER,pCategory);
+        urlBuilder.addQueryParameter(Constants.TYPE_PARAMETER,pType);
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder().url(url).build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 }

83949097ecdc4122c533d983b923dfcbb04173dc
Yonkokilasi <yonkokilasi@gmail.com>
2017-06-20 12:05:36 +0300

    Setup of files Setup of main activity

---
+++ app/src/main/java/com/example/bubbles/rembesha/Constants.java
@@ -0,0 +1,8 @@
+package com.example.bubbles.rembesha;
+
+/**
+ * Created by bubbles on 6/20/17.
+ */
+
+public class Constants {
+}
---
+++ app/src/main/java/com/example/bubbles/rembesha/MakeupService.java
@@ -0,0 +1,13 @@
+package com.example.bubbles.rembesha;
+
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+/**
+ * Created by bubbles on 6/20/17.
+ */
+
+public class MakeupService {
+    public static final findMakeUp(String brand, String pType, String pCategory, ItemTouchHelper.Callback callback) {
+
+    }
+}

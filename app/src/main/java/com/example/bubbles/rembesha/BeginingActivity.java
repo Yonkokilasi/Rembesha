d976258c0c8d18e54d43c0021db7630a39731549
Yonkokilasi <yonkokilasi@gmail.com>
2017-06-22 11:16:05 +0300

    Added begin activity and made it launcher activity . Changed various colors. Added buttons with onclick listeners  on the activity

--- app/src/main/java/com/example/bubbles/rembesha/BeginingActivity.java
+++ app/src/main/java/com/example/bubbles/rembesha/BeginingActivity.java
@@ -1,19 +1,57 @@
 package com.example.bubbles.rembesha;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class BeginingActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.emailLogin) Button mEmail;
+    @Bind(R.id.googleLogin) Button mGoogle;
+    @Bind(R.id.signupLogin) Button mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_begining);
+        ButterKnife.bind(this);
+        mEmail.setOnClickListener(this);
+        mSignUp.setOnClickListener(this);
+        mGoogle.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
+        if (v == mEmail) {
+            Intent intent = new Intent(BeginingActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            Toast.makeText(BeginingActivity.this, "I love Email", Toast.LENGTH_SHORT).show();
+        }
+        if (v == mSignUp) {
+            Intent intent = new Intent(BeginingActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+            Toast.makeText(BeginingActivity.this, "I see you're new here", Toast.LENGTH_SHORT).show();
+        }
+        if (v == mGoogle) {
+            Toast.makeText(BeginingActivity.this, "Coming Soon", Toast.LENGTH_SHORT).show();
+        }
 
     }
+
+
 }
+
+
